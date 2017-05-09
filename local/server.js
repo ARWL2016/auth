ad536ade9e7b12ad0f8c1542059ab62b48d45f91
Alistair Willis <alistairrwillis@gmail.com>
2017-05-09 13:22:12 +0900

    update

--- local/server.js
+++ local/server.js
@@ -14,30 +14,62 @@ app.set('view engine', 'ejs');
 app.locals.status = 'pending'; 
 
 app.get('/', (req, res) => {
-  res.render('login.ejs', {
-    status: app.locals.status
+  res.render('login', {
+    status: app.locals.status, 
+    error: app.locals.error
   });
+}); 
+
+app.get('/profile', (req, res) => {
+  if (app.locals.status === 'signed in') {
+    res.render('profile', {
+    status: app.locals.status, 
+    user: app.locals.user.username, 
+    password: app.locals.user.password, 
+    id: app.locals.user._id
+  });
+  } else {
+    res.redirect('/'); 
+  } 
+}); 
+
+app.post('/auth/logout', (req, res) => {
+  app.locals.status = 'pending'; 
+  app.locals.user = {}; 
+  app.locals.error = ''; 
+  res.redirect('/'); 
 }); 
 
 app.post('/auth/register', (req, res) => {
   console.log(req.body); 
-  User.create(req.body, (err, data) => {
-    if (err) console.log(err); 
-    if (data) console.log(data); 
-  })
-
-  res.send('Thank you for registering'); 
-
+  if(req.body.username && req.body.password) {
+    User.create(req.body, (err, data) => {
+      if (err) {
+        console.log(err); 
+      }
+      else {
+        console.log(data); 
+        app.locals.status = 'Registered. Please sign in.' 
+        res.redirect('/'); 
+      }  
+    })
+  } else {
+    app.locals.error = 'enter a username and password'; 
+    console.log('error'); 
+    res.redirect('/'); 
+  }
 });  
 
 app.post('/auth/signIn', (req, res) => {
-  User.findOne({username: req.body.username}, (err, data) => { 
-    if (data) {
-      console.log(data); 
-      res.send('Thanks for signing in')
+  User.findOne({username: req.body.username}, (err, user) => { 
+    if (user) {
+      console.log(user); 
+      app.locals.status = 'signed in'; 
+      app.locals.user = user; 
+      res.redirect('/profile'); 
     } else {
       console.log('not found');
-      app.locals.status = 'not found' 
+      app.locals.status = 'not found'; 
       res.redirect('/'); 
       
     }
