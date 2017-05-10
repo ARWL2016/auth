02a095bb18fab52628fd062d3fba73bd3ae1647f
Alistair Willis <alistairrwillis@gmail.com>
2017-05-10 21:56:33 +0900

    add passport

---
+++ passport-local/auth/index.js
@@ -0,0 +1,40 @@
+const passport = require('passport'); 
+const LocalStrategy = require('passport-local').Strategy; 
+const db = require('../db'); 
+const User = require('../db').UserModel; 
+
+module.exports = (app) => {
+  app.use(passport.initialize()); 
+  app.use(passport.session()); 
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id); 
+  });
+
+  passport.deserializeUser((user, done) => {
+  
+    done(null, user); 
+  });
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    console.log('auth function', username, password); 
+    User.findOne({ username: username }, (err, user) => {
+    
+    if (err) {
+      console.log('auth error'); 
+      return done(err);
+    }
+    if (!user) {
+      console.log('auth error - no user');
+      return done(null, false); 
+    }
+    console.log('no error'); 
+    return done(null, user); 
+    })
+  }
+  ));
+
+  
+
+}
+
